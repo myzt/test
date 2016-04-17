b1098c919e03573a8a8ca939f72cee628c6d53fc
myzt <spitzyasu@gmail.com>
2016-04-17 19:16:49 +0900

    Create init.el

---
+++ init.el
@@ -0,0 +1,82 @@
+;; キーバインド
+(global-set-key "\C-h" 'delete-backward-char)
+(define-key global-map (kbd "C-z") 'undo)
+
+;; 環境を日本語、UTF-8にする
+(set-locale-environment nil)
+(set-language-environment "Japanese")
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; 初期画面
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+
+;; 括弧の範囲内を強調表示
+(show-paren-mode t)
+(setq show-paren-delay 0)
+(setq show-paren-style 'expression)
+
+;; 括弧の範囲色
+(set-face-background 'show-paren-match-face "#500")
+
+;; 選択領域の色
+(set-face-background 'region "#555")
+
+;; 行末の空白を強調表示
+(setq-default show-trailing-whitespace t)
+(set-face-background 'trailing-whitespace "#b14770")
+
+;; スペース、タブなどを可視化する
+(global-whitespace-mode 1)
+
+;; タブをスペースで扱う
+(setq-default indent-tabs-mode nil)
+
+;; タブ幅
+(custom-set-variables '(tab-width 4))
+
+;; yes or noをy or n
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; 最近使ったファイルをメニューに表示
+(recentf-mode t)
+
+;; 最近使ったファイルの表示数
+(setq recentf-max-menu-items 10)
+
+;; 最近開いたファイルの保存数を増やす
+(setq recentf-max-saved-items 3000)
+
+;; ミニバッファの履歴を保存する
+(savehist-mode 1)
+
+;; ミニバッファの履歴の保存数を増やす
+(setq history-length 3000)
+
+;; バックアップを残さない
+(setq make-backup-files nil)
+
+;; 終了時にオートセーブファイルを消す
+(setq delete-auto-save-files t)
+
+;; 行間
+(setq-default line-spacing 0)
+
+;; 1行ずつスクロール
+(setq scroll-conservatively 35
+      scroll-margin 0
+      scroll-step 1)
+(setq comint-scroll-show-maximum-output t) ;; shell-mode
+
+;; C-Ret で矩形選択
+;; 詳しいキーバインド操作：http://dev.ariel-networks.com/articles/emacs/part5/
+(cua-mode t)
+(setq cua-enable-cua-keys nil)
+
+;; Macのoptionをメタキーにする
+(setq mac-option-modifier 'meta)
